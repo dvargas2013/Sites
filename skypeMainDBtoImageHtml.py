3125f61674e429f15734db2f638b3c29a84f9be0
Daniel Vargas <dvargas2013@gmail.com>
2016-11-29 12:11:42 -0500

    Made a few changes in telephone And Edited the program to find images in Skype's main.db

--- skypeMainDBtoImageHtml.py
+++ skypeMainDBtoImageHtml.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python3
 
 '''
-After running "sqlite3 main.db" and pasting
-----------------
+select skypename,mood_text from Contacts;
 .mode column
 .header on
 .output file.txt
-select convo_id,chatname,author,timestamp,type,body_xml from messages
-where chatname == "karkitty95" and type=201 and author == "karkitty95"
-order by id desc;
------------------
-into the console. it will make file.txt.
 
-that is when u run this to make the html file.
+https://login.skype.com/login
 '''
 
-from done.File import read,write
+name = 'karkitty95'
+limit = 200 # 0 means gimme all of em; any limit is that limit
+oldToNew = 0 # true / false
 
-s = read("file.txt").replace("imgt1","imgpsh_fullsize")
-
+from subprocess import check_output
+from done.File import write
 from re import findall
 
-write("<style>img {height:25%;} a{} div {display:inline}</style>" + '\n'.join('<div><a href=%s><img src= %s /></a></div>'%(i,i) for i in findall('(?<=url_thumbnail=)"[^"]*"',s)),
-'links.html')
+s = check_output(["sqlite3","main.db","-batch",
+'select convo_id,chatname,author,timestamp,type,body_xml from messages where chatname == %r and type=201 and author == %r order by id '%(name,name) + ('asc' if oldToNew else 'desc') + ((' limit %d'%limit) if limit else '') + ";"])
+if type(s) == bytes: s = s.decode()
+s = s.replace("imgt1","imgpsh_fullsize")
+print(s.split('\n')[0])
+write("<style>img {height:25%;} a{} div {display:inline}</style>" + '\n'.join('<div><a href=%s><img src= %s /></a></div>'%(i,i) for i in findall('(?<=url_thumbnail=)"[^"]*"',s)),'links.html')
