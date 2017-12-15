852ee96d984f01e288ee4f8efa402d8bc9b3e158
Daniel Vargas <dvargas2013@gmail.com>
2017-12-15 12:42:07 -0500

    Added a Cookie for Background Color

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 <?if (!empty($header_data)) echo $header_data;
 if (!empty($header_file) && file_exists($header_file)) include $header_file;?>
 </head>
-<body>
+<body <? if ($_COOKIE["bodyBackgroundColor"]) echo "style=\"background-color:".$_COOKIE['bodyBackgroundColor']."\""; ?> >
 <H1 style='background-color: <?php echo "hsl(".$c=rand(0,360).",50%,75%)"?>;' ><a href="/">Home</a>
 <?php
 $sect=explode("/",trim($_SERVER['REQUEST_URI'],"/"));
@@ -19,6 +19,20 @@ foreach ($sect as $index => $string) {
 	}
 }
 ?>
+<input id="svgColorPicker" style="display:none"type="color"
+oninput="document.body.style='background:'+this.value; saveBodyBackground(this.value)"></input>
+<svg preserveAspectRatio="xMidYMid meet" style="width: 1em;height: 1em;float: right;padding: .5em;cursor: pointer;" viewBox="0 0 25 25"
+onclick="this.previousElementSibling.click()">
+<!-- I stole this from YouTube pls no sue me D: -->
+<g><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path></g></svg>
+<script>
+function saveBodyBackground(color) {
+	document.cookie = "bodyBackgroundColor="+color;
+	// we use cookies when we want the server to know
+	// localStorage.bodyBackgroundColor = color;
+	// we use local storage when thte server doesnt need to noe ur data lol
+}
+</script>
 </H1>
 <?php
 if (!empty($description_data)) {
