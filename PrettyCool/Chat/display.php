fc3caef4eb1ee561ff3b7b90c9b70e8585eb02c9
Daniel Vargas <dvargas2013@gmail.com>
2019-06-07 16:19:44 -0400

    Removed Everything that is on the heroku git so i dont have to update both gits every edit

--- PrettyCool/Chat/display.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (!empty($_GET[name]) && !empty($_GET[message])) {
-
-$name = preg_replace("/[^a-z1-9\-]/",'',strtolower($_GET[name]));
-$message = preg_replace("/[^a-zA-Z1-9\-\s]/",'',strtolower(urldecode($_GET[message])));
-
-echo shell_exec("ChainedTalker/Chain/talker.shexe $name \"$message\"");
-}?>
